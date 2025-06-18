18ccf5fedc5e2e2d053d4c45dee60130d24e1ee2
Akira Chen <man4781747@gmail.com>
2025-06-18 14:47:25 +0800

    讓測試模式會依模式選用而調用不同的js檔案

--- vite.demo.config.js
+++ vite.demo.config.js
@@ -1,3 +1,7 @@
+/**
+ * 輸出 Demo 網頁用的設定
+ */
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
@@ -12,6 +16,23 @@ export default defineConfig({
         postcssNesting
       ],
     },
+  },
+  resolve: {
+    alias: {
+      // 根據環境變數決定使用打包後的模組還是本地組件
+      // Use built module or local component based on environment variable
+      'akira-c-popout-card': process.env.USE_BUILT_MODULE === 'true'
+        ? () => {
+          let targetPath = path.resolve(__dirname, 'lib/popout-card.js')
+          console.log("[模擬正式使用] akira-c-popout-card 模組路徑導向至:", targetPath)
+          return targetPath
+        }
+        : () => {
+          let targetPath = path.resolve(__dirname, 'src/components/PopoutCard')
+          console.log("[開發模式使用] akira-c-popout-card 模組路徑導向至:",targetPath)
+          return targetPath
+        },
+    }
   },
   build: {
     outDir: 'dist', // 避免污染 lib/
